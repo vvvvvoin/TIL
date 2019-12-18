940f60a90ffdb4d804c057fc0daa4acb4389843e
vvvvvoin <aszx7009@gmail.com>
2019-12-18 16:35:56 +0900

    반복문

---
+++ python/06_loop.py
@@ -0,0 +1,9 @@
+menus = ["순남시래기", "양자강", "20층", "버거킹"]
+
+for menu in menus:
+    print(menu)
+print(type(menus))
+print("==============================================")
+for i in range(4):
+    print(i)
+    print(menus[i])
