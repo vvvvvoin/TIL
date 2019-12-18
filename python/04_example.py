a200612cd0c673df437cd10501669e5d717cce21
vvvvvoin <aszx7009@gmail.com>
2019-12-18 15:08:50 +0900

    실습 완료

---
+++ python/04_example.py
@@ -0,0 +1,30 @@
+# print() happy hacking!
+hphk = "해피해킹"
+
+print(hphk)
+print(hphk)
+print(hphk)
+print(hphk)
+print(hphk)
+
+# 점심메뉴판
+menu1 = "순남 시래기"
+menu2 = "양자강"
+menu3 = "20층"
+print(menu1)
+print(menu2)
+print(menu3)
+
+# List
+menus = ["순남시래기", "양자강", "20층"]
+print(menus)
+print(menus[0])
+
+# 전화번호부 저장하기
+phone_nums = ["02-111-1111", "02-2222-2222", "02-333-3333"]
+print(phone_nums[0])
+
+# dictionary
+dict_nums = {"순남 시래기" : "02-456-8647", "양자강" : "02-468-8971", "20층" : "02-785-3843"}
+
+print(dict_nums["순남 시래기"])
